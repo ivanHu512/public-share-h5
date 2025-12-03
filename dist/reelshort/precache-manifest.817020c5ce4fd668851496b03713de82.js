cc65cd7319e767551e47442db107ba24cc77a287
胡帅 <hushuai@crazymaplestudio.com>
2025-12-03 19:31:33 +0800

    提交版本

---
+++ dist/reelshort/precache-manifest.817020c5ce4fd668851496b03713de82.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d7450539e2049268b532b5f89521da",
+    "url": "..//reelshort/static/media/busy.d4d74505.svg"
+  },
+  {
+    "revision": "a9f9907ef5f3ffdde3b4a8c032666878",
+    "url": "..//reelshort/static/media/default.a9f9907e.svg"
+  },
+  {
+    "revision": "8f5ec4b82c8561c66195d21d01c2fd90",
+    "url": "..//reelshort/static/media/disconnected.8f5ec4b8.svg"
+  },
+  {
+    "revision": "a16d61c992ffdd4b294146e5ebc43d15",
+    "url": "..//reelshort/static/media/empty-icon.a16d61c9.svg"
+  },
+  {
+    "revision": "2507a6f6dce2f70c202274cd841ca145",
+    "url": "..//reelshort/static/media/empty.2507a6f6.svg"
+  },
+  {
+    "revision": "aaabceac38ea4a2a8d8fde649ab26320",
+    "url": "..//reelshort/static/media/selector-check-mark.aaabceac.svg"
+  },
+  {
+    "revision": "387022ecda55d19c7a17c77486cb6fe3",
+    "url": "..//reelshort/static/media/slider-thumb.387022ec.svg"
+  },
+  {
+    "revision": "f1032cee00c86570e74fee4aa6bd6427",
+    "url": "..//reelshort/static/media/spin.f1032cee.svg"
+  },
+  {
+    "revision": "298dbdd63e3bac23e748",
+    "url": "../blackFridayPage/blackFridayPage.5edc5c90.css"
+  },
+  {
+    "revision": "298dbdd63e3bac23e748",
+    "url": "../blackFridayPage/blackFridayPage.abe4e67f.js"
+  },
+  {
+    "revision": "f398d184a3655f687fb1c207b5a647e3",
+    "url": "../blackFridayPage/blackFridayPage.abe4e67f.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2e8d3c47825b9827cbc1b62e4ebccf",
+    "url": "../blackFridayPage/index.html"
+  },
+  {
+    "revision": "d1fcf082816ab189b518",
+    "url": "../booksAdvPage/booksAdvPage.57925196.css"
+  },
+  {
+    "revision": "d1fcf082816ab189b518",
+    "url": "../booksAdvPage/booksAdvPage.d08a9d6c.js"
+  },
+  {
+    "revision": "1252d137c4310db32b0f116f30511532",
+    "url": "../booksAdvPage/booksAdvPage.d08a9d6c.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c83d712b1d10a7116156cc641e6661",
+    "url": "../booksAdvPage/index.html"
+  },
+  {
+    "revision": "8e00cdc8994936be55fe",
+    "url": "../booksAdvPageV2/booksAdvPageV2.0c9ed0dd.js"
+  },
+  {
+    "revision": "1252d137c4310db32b0f116f30511532",
+    "url": "../booksAdvPageV2/booksAdvPageV2.0c9ed0dd.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e00cdc8994936be55fe",
+    "url": "../booksAdvPageV2/booksAdvPageV2.c93aac00.css"
+  },
+  {
+    "revision": "2533adf456f724eb1541f887b88fc00c",
+    "url": "../booksAdvPageV2/index.html"
+  },
+  {
+    "revision": "f6d625f52b195244cdc7",
+    "url": "../christmasPage/christmasPage.05e2cbcb.css"
+  },
+  {
+    "revision": "f6d625f52b195244cdc7",
+    "url": "../christmasPage/christmasPage.4884aeea.js"
+  },
+  {
+    "revision": "f398d184a3655f687fb1c207b5a647e3",
+    "url": "../christmasPage/christmasPage.4884aeea.js.LICENSE.txt"
+  },
+  {
+    "revision": "102cf478b38970b7ea23f5d7543ea4bd",
+    "url": "../christmasPage/index.html"
+  },
+  {
+    "revision": "8ee5cd8c10ecaaa50525",
+    "url": "../collaborationPage/collaborationPage.1ca7d801.js"
+  },
+  {
+    "revision": "e3899931f4bd26a1ae16d6f85f9d31d8",
+    "url": "../collaborationPage/collaborationPage.1ca7d801.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee5cd8c10ecaaa50525",
+    "url": "../collaborationPage/collaborationPage.78920f9e.css"
+  },
+  {
+    "revision": "f2b70707c99d3f50d38f1d1fdf656cf1",
+    "url": "../collaborationPage/index.html"
+  },
+  {
+    "revision": "3b7a41d6f3d3586ffed3",
+    "url": "../createForUs/createForUs.df1c88c9.js"
+  },
+  {
+    "revision": "3b7a41d6f3d3586ffed3",
+    "url": "../createForUs/createForUs.faa489b2.css"
+  },
+  {
+    "revision": "92caec4749756ea9e5c02d1d3a715f92",
+    "url": "../createForUs/index.html"
+  },
+  {
+    "revision": "c44653478d9fd505d7a6",
+    "url": "../emailVerificationPage/emailVerificationPage.460cf20d.js"
+  },
+  {
+    "revision": "5ca24a92fb2f15455e2619ed0f55ea53",
+    "url": "../emailVerificationPage/emailVerificationPage.460cf20d.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44653478d9fd505d7a6",
+    "url": "../emailVerificationPage/emailVerificationPage.82ddbc88.css"
+  },
+  {
+    "revision": "50e699a8276fa13698d4291f7761d3c4",
+    "url": "../emailVerificationPage/index.html"
+  },
+  {
+    "revision": "b9303102fbe67d542920",
+    "url": "../halloweenEventPage/halloweenEventPage.3952a3e3.js"
+  },
+  {
+    "revision": "f398d184a3655f687fb1c207b5a647e3",
+    "url": "../halloweenEventPage/halloweenEventPage.3952a3e3.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9303102fbe67d542920",
+    "url": "../halloweenEventPage/halloweenEventPage.4f506df4.css"
+  },
+  {
+    "revision": "7c5c50f92e070dbf3edac3a1d537860c",
+    "url": "../halloweenEventPage/index.html"
+  },
+  {
+    "revision": "a495a166a257bbd12da750615239b5e3",
+    "url": "../joinUsPage/index.html"
+  },
+  {
+    "revision": "21a8a6e2db7b4b4fb39e",
+    "url": "../joinUsPage/joinUsPage.69f1a8fb.css"
+  },
+  {
+    "revision": "21a8a6e2db7b4b4fb39e",
+    "url": "../joinUsPage/joinUsPage.e4e894e0.js"
+  },
+  {
+    "revision": "e3899931f4bd26a1ae16d6f85f9d31d8",
+    "url": "../joinUsPage/joinUsPage.e4e894e0.js.LICENSE.txt"
+  },
+  {
+    "revision": "96296cb99c3aa6ef0330f153250cf7b1",
+    "url": "../paypalPage/index.html"
+  },
+  {
+    "revision": "ebfee6b9461f08d70641",
+    "url": "../paypalPage/paypalPage.18a979f9.css"
+  },
+  {
+    "revision": "ebfee6b9461f08d70641",
+    "url": "../paypalPage/paypalPage.b7bb88fe.js"
+  },
+  {
+    "revision": "b22bf62355e2f4f440e5f1db2a52e888",
+    "url": "../shareBookPage/index.html"
+  },
+  {
+    "revision": "d362e0802ec20c4c708b",
+    "url": "../shareBookPage/shareBookPage.15ba5440.css"
+  },
+  {
+    "revision": "d362e0802ec20c4c708b",
+    "url": "../shareBookPage/shareBookPage.afdeba0c.js"
+  },
+  {
+    "revision": "f398d184a3655f687fb1c207b5a647e3",
+    "url": "../shareBookPage/shareBookPage.afdeba0c.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab5230af2fcdbb24405",
+    "url": "../static/js/0.a4ead203.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "../static/js/0.a4ead203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029ab0b983f115d6c83d",
+    "url": "../static/js/1.3e3cd86f.chunk.js"
+  },
+  {
+    "revision": "99b52b7ce7c67c22100b4fd6d1f8a8ac",
+    "url": "../static/media/SofiaPro.99b52b7c.ttf"
+  },
+  {
+    "revision": "f29ba56effd9292990aa0ca2eca0687f",
+    "url": "../static/media/bottom.f29ba56e.png"
+  },
+  {
+    "revision": "8bfaf31163eacf4a108e7b6280cd6f39",
+    "url": "../static/media/grid.8bfaf311.png"
+  },
+  {
+    "revision": "aec67bca1bf380cfe439ad1a13f50aea",
+    "url": "../static/media/group1.aec67bca.png"
+  },
+  {
+    "revision": "ac01c794b9b272a0e802cdb0ba657e1d",
+    "url": "../static/media/group2.ac01c794.png"
+  },
+  {
+    "revision": "d4545fca674f0c7785ca53cf2a26d6ed",
+    "url": "../static/media/header.d4545fca.png"
+  },
+  {
+    "revision": "90e397b4fbf1195033edc494b376043e",
+    "url": "../static/media/icon_finger.90e397b4.png"
+  },
+  {
+    "revision": "a98e60746a2ace48e8a6a5e5f97a8a74",
+    "url": "../static/media/logo.a98e6074.png"
+  },
+  {
+    "revision": "c71c4485f894b241f27fbbaee3d4d3bd",
+    "url": "../storePage/index.html"
+  },
+  {
+    "revision": "6b98ca192b751880b94b",
+    "url": "../storePage/storePage.47397902.css"
+  },
+  {
+    "revision": "6b98ca192b751880b94b",
+    "url": "../storePage/storePage.920b3d60.js"
+  },
+  {
+    "revision": "1252d137c4310db32b0f116f30511532",
+    "url": "../storePage/storePage.920b3d60.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b90bfca2feb930d29e6f5388bcdce5",
+    "url": "../valentineActivities/index.html"
+  },
+  {
+    "revision": "9fb81731155d7916d98b",
+    "url": "../valentineActivities/valentineActivities.11ceb5bf.css"
+  },
+  {
+    "revision": "9fb81731155d7916d98b",
+    "url": "../valentineActivities/valentineActivities.18aea339.js"
+  },
+  {
+    "revision": "f398d184a3655f687fb1c207b5a647e3",
+    "url": "../valentineActivities/valentineActivities.18aea339.js.LICENSE.txt"
+  },
+  {
+    "revision": "f348302f778ca73caa4081e9c183a95b",
+    "url": "../videoAdvPage/index.html"
+  },
+  {
+    "revision": "e50d822887b8fcba7f6c",
+    "url": "../videoAdvPage/videoAdvPage.8cff144f.js"
+  },
+  {
+    "revision": "1252d137c4310db32b0f116f30511532",
+    "url": "../videoAdvPage/videoAdvPage.8cff144f.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50d822887b8fcba7f6c",
+    "url": "../videoAdvPage/videoAdvPage.cf023084.css"
+  },
+  {
+    "revision": "37c727477c24210f8732c7632c474b9d",
+    "url": "../videoAdvPageV2/index.html"
+  },
+  {
+    "revision": "199f3992d92fc1bec386",
+    "url": "../videoAdvPageV2/videoAdvPageV2.37114693.js"
+  },
+  {
+    "revision": "1252d137c4310db32b0f116f30511532",
+    "url": "../videoAdvPageV2/videoAdvPageV2.37114693.js.LICENSE.txt"
+  },
+  {
+    "revision": "199f3992d92fc1bec386",
+    "url": "../videoAdvPageV2/videoAdvPageV2.b08065dd.css"
+  },
+  {
+    "revision": "f22201ecbc8fee1b290ae4a71c1dd19e",
+    "url": "../videoAdvPageV3/index.html"
+  },
+  {
+    "revision": "ce46bedf4a40522580d7",
+    "url": "../videoAdvPageV3/videoAdvPageV3.b19a10aa.css"
+  },
+  {
+    "revision": "ce46bedf4a40522580d7",
+    "url": "../videoAdvPageV3/videoAdvPageV3.e25f586f.js"
+  },
+  {
+    "revision": "1252d137c4310db32b0f116f30511532",
+    "url": "../videoAdvPageV3/videoAdvPageV3.e25f586f.js.LICENSE.txt"
+  },
+  {
+    "revision": "46128be4fb92ef4e6d45b662f9459381",
+    "url": "../welfareRulePage/index.html"
+  },
+  {
+    "revision": "4a5502f578d2625e46c8",
+    "url": "../welfareRulePage/welfareRulePage.22ec4004.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "../welfareRulePage/welfareRulePage.22ec4004.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5502f578d2625e46c8",
+    "url": "../welfareRulePage/welfareRulePage.e788578a.css"
+  }
+]);
